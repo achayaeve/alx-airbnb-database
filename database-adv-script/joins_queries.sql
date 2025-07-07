8ea22e05f55ac2f0603e3f451b3da861bad5f6e3
Achaya Everlyne Zawadi <93661065+achayaeve@users.noreply.github.com>
2025-07-07 16:33:53 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,16 @@
+SELECT
+  Booking.booking_id
+  Booking.property_id
+  Booking.user_id
+  Booking.start_date
+  Booking.end_date
+  Booking.total_price
+  Booking. status
+  Booking.created_at
+FROM
+  Booking
+INNER JOIN 
+  User ON user_id = Booking_id
+;
+
 
