0770baba259987997446e26763c1d7fd6c518328
Achaya Everlyne Zawadi <93661065+achayaeve@users.noreply.github.com>
2025-07-08 14:15:44 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,26 @@
+SELECT 
+  user_id, COUNT(*) AS Total_bookings
+FROM 
+  Bookimg
+GROUP BY 
+  user_id
+;
+
+SELECT 
+    property_id,
+    property_name,
+    Total_bookings,
+    RANK() OVER (ORDER BY Total_bookings DESC) AS Booking_rank
+FROM (
+    SELECT 
+        p.property_id,
+        p.name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM 
+        Property p
+    LEFT JOIN 
+        Booking b ON p.property_id = b.property_id
+    GROUP BY 
+        p.property_id, p.property_name
+) AS subquery;
 
