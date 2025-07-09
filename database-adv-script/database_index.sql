73723703b26ebe1b6d48a21e928d9ec610fde265
Achaya Everlyne Zawadi <93661065+achayaeve@users.noreply.github.com>
2025-07-09 20:30:28 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,8 @@
+```sql
 
+CREATE INDEX idx_bookings_user_id ON Booking(user_id);
+
+CREATE INDEX idx_bookings_property_id ON Booking(Property_id);
+
+CREATE INDEX idx_bookings_date_range ON Booking(start_date, end_date);
+```
